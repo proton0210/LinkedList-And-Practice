a7ca5a8d9022d05ccc2f94b4c66d581be5523bea
Vidit Shah <vidit0210@protonmail.com>
2023-03-16 18:17:50 +0530

    added Test for Insertion and Deletion

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -50,4 +50,32 @@ test("LinkedList", () => {
   expect(list.indexOf("history")).toBeNull();
   expect(list.indexOf("time")).toBe(3);
   expect(list.read(3)).toBe("time");
+
+  // Insert
+  list.insertAtIndex(3, "purple");
+  expect(list.indexOf("purple")).toBe(3);
+  expect(list.read(3)).toBe("purple");
+
+  list.insertAtIndex(0, "red");
+  expect(list.indexOf("red")).toBe(0);
+  expect(list.read(0)).toBe("red");
+
+  list.insertAtIndex(11, "blue");
+  expect(list.indexOf("blue")).toBe(6);
+  expect(list.read(6)).toBe("blue");
+  expect(list.read(7)).toBeNull();
+
+  // Delete
+  list.deleteAtIndex(6);
+  expect(list.indexOf("blue")).toBeNull();
+  expect(list.read(6)).toBeNull();
+
+  expect(list.indexOf("red")).toBe(0);
+  expect(list.read(0)).toBe("red");
+  expect(list.indexOf("once")).toBe(1);
+  expect(list.read(1)).toBe("once");
+
+  list.deleteAtIndex(0);
+  expect(list.indexOf("red")).toBeNull();
+  expect(list.read(0)).toBe("once");
 });
